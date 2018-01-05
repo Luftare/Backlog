77446d3439b54e6c2133b5735f7a303be1277430
Ilmari Koskinen <ilmari.koskinen1@gmail.com>
2018-01-05 11:33:54 +0200

    Create buttons for category deletion

--- client/src/backlog/topics/Topic.js
+++ client/src/backlog/topics/Topic.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Topic = ({topic, selectTopic, selectedTopic}) => {
+const Topic = ({topic, selectTopic, selectedTopic, deleteTopic, deleteVisible}) => {
   const classes = "category__item " + (selectedTopic && selectedTopic.name === topic.name? "category__item--active" : "");
   return (
-    <li className={classes} onClick={e => selectTopic(topic)}>{topic.name}</li>
+    <li className={classes} onClick={e => selectTopic(topic)}>{deleteVisible? <button onClick={e => deleteTopic(e, topic)}>-</button> : null}{topic.name}</li>
   );
 };
 
